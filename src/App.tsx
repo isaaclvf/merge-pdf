b8fcd6d789768033fb5b95b8cdf294c88eb4d7f7
Isaac <isaacfarias006@gmail.com>
2023-03-23 18:13:24 -0300

    Feat: upload pdf files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const newFiles = [...files, ...Array.from(e.dataTransfer.files)];
+    setFiles(newFiles);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newFiles = [...files, ...Array.from(e.target.files || [])];
+    setFiles(newFiles);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="drop-zone"
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={handleDrop}
+      >
+        {files.length > 0 ? (
+          <ul>
+            {files.map((file, index) => (
+              <li key={index}>File {index + 1}: {file.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Drag and drop PDF files here, or click to select files</p>
+        )}
+        <input
+          type="file"
+          accept="application/pdf"
+          multiple
+          onChange={handleFileChange}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
