e1af7bb9dae5d9ff713f259b57afe40b98cbe80f
Isaac <isaacfarias006@gmail.com>
2023-03-25 15:21:39 -0300

    Handle file ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [files, setFiles] = useState<File[]>([]);
@@ -14,6 +15,26 @@ function App() {
     setFiles(newFiles);
   };
 
+  const handleRemoveFile = (index: number) => {
+    const newFiles = [...files];
+    newFiles.splice(index, 1);
+    setFiles(newFiles);
+  };
+
+  const handleMoveUp = (index: number) => {
+    if (index === 0) return;
+    const newFiles = [...files];
+    [newFiles[index - 1], newFiles[index]] = [newFiles[index], newFiles[index - 1]];
+    setFiles(newFiles);
+  };
+
+  const handleMoveDown = (index: number) => {
+    if (index === files.length - 1) return;
+    const newFiles = [...files];
+    [newFiles[index], newFiles[index + 1]] = [newFiles[index + 1], newFiles[index]];
+    setFiles(newFiles);
+  };
+
   return (
     <div className="App">
       <div
@@ -24,7 +45,12 @@ function App() {
         {files.length > 0 ? (
           <ul>
             {files.map((file, index) => (
-              <li key={index}>File {index + 1}: {file.name}</li>
+              <li key={index}>
+                File {index + 1}: {file.name}
+                <button onClick={() => handleMoveUp(index)} disabled={index === 0} >Up</button>
+                <button onClick={() => handleMoveDown(index)} disabled={index === files.length-1}>Down</button>
+                <button onClick={() => handleRemoveFile(index)} >Remove</button>
+              </li>
             ))}
           </ul>
         ) : (
